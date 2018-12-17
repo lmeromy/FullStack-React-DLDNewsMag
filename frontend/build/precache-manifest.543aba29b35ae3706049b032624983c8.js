be12b63150af643d73e4458604213aa275b69735
Leah Meromy <lmeromy@gmail.com>
2018-12-17 15:11:30 +0000

    Backend and frontend repos merged together into one repository

---
+++ frontend/build/precache-manifest.543aba29b35ae3706049b032624983c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2796b55102ba4db7a952",
+    "url": "/static/js/main.2796b551.chunk.js"
+  },
+  {
+    "revision": "b947b0cc33c112fa647f",
+    "url": "/static/js/1.b947b0cc.chunk.js"
+  },
+  {
+    "revision": "2796b55102ba4db7a952",
+    "url": "/static/css/main.c834f738.chunk.css"
+  },
+  {
+    "revision": "8adad4d849b16487f93e7298ad330bba",
+    "url": "/index.html"
+  }
+];
